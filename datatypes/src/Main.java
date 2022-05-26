dacc2963762095d5daff1c0e4f93cc5eb2ef25c5
Velihan <velihangozek@hotmail.com>
2022-05-26 23:01:05 +0300

    Add files via upload

---
+++ datatypes/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+        double sayi = 12.4;
+        // sayi = "Ankara"; Can't be done because there are incompatible types.
+        sayi = -129;
+
+        String sehir = "GAZİANTEP";
+        char karakter = 'b'; //holds the character's ascii code.
+
+        boolean dogruMu = true;
+        dogruMu = false;
+
+        if(dogruMu) //if inside the brackets == true, the block will be run.
+        {
+
+        }
+
+    }
+}
